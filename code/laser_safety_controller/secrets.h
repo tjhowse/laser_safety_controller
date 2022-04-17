01cd99dcdcdeefe9b605ba29adf024bf47956b74
tjhowse <tjhowse@gmail.com>
2022-04-17 12:09:53 +1000

    Template secrets file.

---
+++ code/laser_safety_controller/secrets.h
@@ -0,0 +1,5 @@
+static const char *wifi_ssid = "ssid";
+static const char *wifi_password = "password";
+static const char *mqtt_hostname = "server.mqtt.com";
+static const char *mqtt_username = "username";
+static const char *mqtt_password = "password";
